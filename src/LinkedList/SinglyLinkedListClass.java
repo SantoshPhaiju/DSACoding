c4e80e26f1f7d538f2846ab4dbbf55207c7a9a9b
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-01 22:32:45 +0545

    LinkedList implementation starting.

---
+++ src/LinkedList/SinglyLinkedListClass.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+class Node {
+    int data;
+    int next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+class SinglyLinkedList {
+
+}
+
+public class SinglyLinkedListClass {
+    public static void main(String[] args) {
+
+    }
+}
