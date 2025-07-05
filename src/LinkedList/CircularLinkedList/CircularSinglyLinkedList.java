38cb93594da2af48b0d5f270ba26cc82ebfeff58
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-07-05 11:55:10 +0545

    singly circular linked list insertAtFront done.

--- src/LinkedList/CircularLinkedList/CircularSinglyLinkedList.java
+++ src/LinkedList/CircularLinkedList/CircularSinglyLinkedList.java
@@ -34,6 +34,34 @@ class SinglyCircularLinkedList {
         size++;
     }
 
+    public void insertAtFront(int data) {
+        Node2 newNode = new Node2(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            newNode.next = newNode;
+            size++;
+        } else {
+            newNode.next = head;
+            head = newNode;
+            tail.next = head;
+            size++;
+        }
+
+    }
+
+    public void printList() {
+        Node2 temp = head;
+
+        System.out.println("Printing circular singly linked list");
+        for (int i = 0; i < size; i++) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.print(temp.data);
+        System.out.println();
+    }
+
 
 }
 
@@ -42,6 +70,14 @@ public class CircularSinglyLinkedList {
         SinglyCircularLinkedList list = new SinglyCircularLinkedList();
         list.insertAtEnd(1);
         list.insertAtEnd(2);
+        list.insertAtEnd(3);
+        list.insertAtEnd(4);
+
+        list.insertAtFront(5);
+        list.insertAtFront(6);
+
+        list.printList();
+
 
     }
 }
